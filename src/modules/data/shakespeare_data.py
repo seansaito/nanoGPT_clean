5ab4a608df6f5341d2acf607e2c341203a2768aa
Sean Saito <saitosean@ymail.com>
2023-07-26 15:17:05 +0900

    Implemented simple data pipeline

---
+++ src/modules/data/shakespeare_data.py
@@ -0,0 +1,81 @@
+"""
+Based on https://github.com/karpathy/nanoGPT/tree/master/data
+
+"""
+
+import logging
+
+import numpy as np
+import requests
+import tiktoken
+
+from src import DATA_DIR
+from src.utils import gen_path
+
+SHAKESPEARE_DATA_URL = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+DATASET_NAME = "tiny_shakespeare"
+
+logger = logging.getLogger(__name__)
+
+
+def download_data(url: str = SHAKESPEARE_DATA_URL) -> str:
+    """
+    Download shakespeare data
+    """
+
+    logger.info("Fetching data from {}".format(url))
+    response = requests.get(url).text
+
+    path_raw_input = gen_path(
+        path_dir=DATA_DIR / "input" / DATASET_NAME, fname="input.txt", timestamp=False
+    )
+    logger.info("Writing to {}".format(path_raw_input))
+    with open(path_raw_input, "w") as fp:
+        fp.write(response)
+
+    return path_raw_input
+
+
+def encode_data(path: str, train_ratio: float = 0.9) -> dict[str, str]:
+    """
+    Encode the data and return the files
+    """
+    # Read the data
+    with open(path, "r") as fp:
+        data = fp.read()
+
+    # Split data into train and test
+    num_lines = len(data)
+    train_data = data[: int(num_lines * train_ratio)]
+    val_data = data[int(num_lines * (1 - train_ratio)) :]
+
+    # Encode using tiktoken GPT-2 BPE
+    encoder = tiktoken.get_encoding("gpt2")
+    train_ids = encoder.encode_ordinary(train_data)
+    val_ids = encoder.encode_ordinary(val_data)
+
+    logger.info(f"Train has {len(train_ids):,} tokens")
+    logger.info(f"Test has {len(val_ids):,} tokens")
+
+    # Export
+    arr_train_ids = np.array(train_ids, dtype=np.uint16)
+    arr_val_ids = np.array(val_ids, dtype=np.uint16)
+
+    path_train_save = gen_path(
+        path_dir=DATA_DIR / "dataset" / DATASET_NAME, fname="train.bin", timestamp=False
+    )
+
+    path_val_save = gen_path(
+        path_dir=DATA_DIR / "dataset" / DATASET_NAME, fname="val.bin", timestamp=False
+    )
+
+    logger.info("Saving training data to: {}".format(path_train_save))
+    arr_train_ids.tofile(path_train_save)
+
+    logger.info("Saving validation data to: {}".format(path_val_save))
+    arr_val_ids.tofile(path_val_save)
+
+    return {
+        "train_ids": path_train_save,
+        "val_ids": path_val_save,
+    }
--- src/pipelines/pipeline_prepare_data.py
+++ src/pipelines/pipeline_prepare_data.py
@@ -1,12 +1,23 @@
+"""
+Pipeline for preparing dataset
+"""
+
 import argparse
 import logging
 
+from src.modules.data import shakespeare_data
 from src.utils import set_logging, timeit
 
 
 @timeit
-def main():
-    ...
+def main() -> dict[str, str]:
+    # Download shakespeare data
+    path_raw_data = shakespeare_data.download_data()
+
+    # Create dataset
+    dict_path_dataset = shakespeare_data.encode_data(path=path_raw_data)
+
+    return dict_path_dataset
 
 
 if __name__ == "__main__":
